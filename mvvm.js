a553d2912b7a4ead4790c39dee8826ffc1250f25
xiehao <xiehao@phoenixtv.com>
2019-12-03 21:33:37 +0800

    数据与视图绑定

--- mvvm.js
+++ mvvm.js
@@ -21,11 +21,13 @@ function Mvvm(option = {}) {
 function observe(data) {
     if (data != null && typeof data === 'object') {
         for(let key in data) {
+            let dep = new Dep()
             let val = data[key];
             observe(val); //递归劫持
             Object.defineProperty(data, key, {
                 enumerable: true,
                 get() {
+                    Dep.target && dep.addSub(Dep.target);
                     return val;
                 },
                 set(newVal) {
@@ -34,6 +36,7 @@ function observe(data) {
                         if (val != null && typeof val === 'object') {
                             observe(val); //新值数据劫持
                         }
+                        dep.notify();
                     } else {
                         return;
                     }
@@ -59,11 +62,15 @@ function compile(el, vm) {
             console.log(text)
             let reg = /\{\{(.*)\}\}/;
             if (node.nodeType === 3 && reg.test(text)) {
-                let arr = RegExp.$1.split('.');
-                console.log(arr)
-                let val = vm;
-                arr.forEach(key => {
-                    val = val[key];
+                // let arr = RegExp.$1.split('.'); //获取页面数据属性
+                // console.log(arr)
+                // let val = vm;
+                // arr.forEach(key => {
+                //     val = val[key];
+                // })
+                let val = getProperty(vm, RegExp.$1)
+                new Watcher(vm, RegExp.$1, function(newVal){
+                    node.textContent = text.replace(reg, newVal);
                 })
                 node.textContent = text.replace(reg, val);
             }
@@ -74,6 +81,65 @@ function compile(el, vm) {
     }
     vm.$el.appendChild(fragment); //回写到页面
 }
+
+
+
+function getProperty(vm, exp) {
+    let val = vm;
+    let arr = exp.split('.');
+    arr.forEach(key => {
+        val = val[key]
+    })
+    return val;
+}
+
+
+//发布、订阅
+function Dep() {
+    this.subs = [];
+}
+
+Dep.prototype.addSub = function(sub) {
+    this.subs.push(sub);
+}
+
+Dep.prototype.notify = function(sub) {
+    this.subs.forEach(sub => sub.update())
+}
+
+function Watcher(vm, exp, fn) {
+    this.vm = vm;
+    this.exp = exp;
+    Dep.target = this;
+    // let val =vm;
+    // let arr = exp.split('.');
+    // arr.forEach(key => {
+    //     val = val[key]
+    // })
+    let val = getProperty(vm, exp)
+    Dep.target = null;
+    this.fn = fn
+}
+
+Watcher.prototype.update = function() {
+    let val = getProperty(this.vm, this.exp)
+    this.fn(val);
+}
+
+// let watcher1 = new Watcher(function() {
+//     console.log('aa')
+// })
+
+// let watcher2 = new Watcher(function() {
+//     console.log('bb')
+// })
+
+// let dep = new Dep();
+// console.log(dep)
+// dep.addSub(watcher1)
+// dep.addSub(watcher2)
+// dep.notify()
+
 
 // function observe(data) {
 //     return new Observe(data);
